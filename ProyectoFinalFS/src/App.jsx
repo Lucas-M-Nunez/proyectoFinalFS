58f4ec7c111f3eeef86a4294204fb8e88c41cd0e
Lucas M. Nuñez <114788609+Lucas-M-Nunez@users.noreply.github.com>
2026-04-30 12:04:54 -0300

    Create proyecto

---
+++ ProyectoFinalFS/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() { // Componente funcional, es una función que retorna un elemento de React (JSX)
+  let nombre = "Lucas";
+
+  return <h2>Hola {nombre}</h2>;
+}
+
+export default App
